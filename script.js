6cce3b0b326da416a0e547d11a067f3e827a940a
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-01-25 15:57:04 -0700

    bonus function

--- script.js
+++ script.js
@@ -29,7 +29,30 @@ var swapCase = function(string){
 var text = 'Hello World'
 swapCase(text);
 
-//var letterCount = function(){
-//    
-//}
 
+
+var letterCount = function(string) {
+	var counter = 0;
+	var greatest = "";
+	var sArray = string.split(" ");
+	for(i = 0; i < sArray.length; i++) {
+		var innerCounter = 0
+		var letters = sArray[i].split("")
+		for(j = 0; j < letters.length; j++) {
+			for(k = (j + 1); k < letters.length; k++) {
+				if(letters[j] === letters[k]) {
+					innerCounter += 1
+				}
+			}
+		}
+		if(innerCounter > counter) {
+			counter = innerCounter;
+			innerCounter = 0;
+			greatest = sArray[i];
+		}	
+	}
+	return greatest;
+}
+
+var text = 'Which word has the most rerererepeating letters'
+console.log(letterCount(text));
